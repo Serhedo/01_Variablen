c50f3ec982e9aa4e8040118543bc5b570740dca0
Serhedo <serhat.alpayci@gmx.de>
2023-05-09 11:42:09 +0200

    Variablen update

--- script_01.js
+++ script_01.js
@@ -15,3 +15,42 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
+// let firstName; // Deklaration (Definition)
+// firstName = "Max";
+// console.log(firstName); // Ausgabe
+
+// let familyName;
+// familyName = "Mütze";
+// console.log(familyName);
+
+/***** 03 Deklaration + Wertzuweisung II *******/
+
+// let firstName, familyName;
+
+// firstName= prompt("Bitte Vornamen eingeben");
+// familyName= prompt("Bitte Nachnamen eingeben");
+
+// console.log(firstName + " " + familyName);
+// console.log(typeof firstName);
+
+
+
+// let test;
+// test = "Max"; //string
+// test = 2; //number
+// test = true; //boolean
+
+// console.log ("Datentyp: " + typeof test);
+// console.log ("Inhalt: " + test);
+
+/***** 03a Konstanten *******/
+
+let test ="Max"; // Deklaration + Wertzuweisung
+test = "Maxine";
+
+console.log("Inhalt: " + test);
+
+
+
+
+
